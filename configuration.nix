648a42b3db86b08e8cd93de72c9a3931751ccc74
cullvox <cullvox@outlook.com>
2022-11-04 22:35:57 -0400

    Adding nix file

---
+++ configuration.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, inputs, ... }:
+{
+	# Enable the usage of proprietary/unfree packages repository.
+	nixpkgs.config.allowUnfree = true;
+	
+	# Enable flakes
+	nix = {
+		package = pkgs.nixVersions.unstable;
+		registry.nixpkgs.flake = inputs.nixpkgs;
+		settings = {
+			auto-optimise-store = true;
+			trusted-users = [ "root" "cullvox" ];
+		};
+		extraOptions = ''
+			experimental-features = nix-command flakes
+		'';
+		gc = {
+			automatic = true;
+			options = "-d";
+		}
+	}
+}
